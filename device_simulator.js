f8d895db40411ff9bd7a824fdde46e936afb2b2f
bohdank <koldunbohdan@gmail.com>
2024-05-28 15:04:34 +0300

    init

--- device_simulator.js
+++ device_simulator.js
@@ -1,14 +1,16 @@
 const Bacnet = require("bacstack");
 
-const settings = {
-    deviceId: 443,
-    vendorId: 7
-};
-
 const client = new Bacnet({
     port: 47808,
 });
 
+// Define the device settings
+const settings = {
+    deviceId: 443,  // Unique device ID
+    vendorId: 7    // Vendor ID
+};
+
+// Define the data store with object properties
 const dataStore = {
     '1:0': {
         75: [{value: {type: 1, instance: 0}, type: 12}],    // PROP_OBJECT_IDENTIFIER
@@ -28,13 +30,16 @@ const dataStore = {
     }
 };
 
+// Event listener for 'iAm' messages
 client.on('iAm', (device) => {
-    console.log('Received iAm:', device);
+    console.log('Received whoIs request:', device);
 });
 
+// Event listener for 'readProperty' requests
 client.on('readProperty', (data) => {
+    console.log('Received readProperty request:', data);
+
     const object = dataStore[data.request.objectId.type + ':' + data.request.objectId.instance];
-    console.log('object', object);
     if (!object) return client.errorResponse(data.address, Bacnet.enum.ConfirmedServices.SERVICE_CONFIRMED_READ_PROPERTY, data.invokeId, Bacnet.enum.ErrorClasses.ERROR_CLASS_OBJECT, Bacnet.enum.ErrorCodes.ERROR_CODE_UNKNOWN_OBJECT);
     const property = object[data.request.property.id];
     console.log('object', property);
@@ -48,7 +53,10 @@ client.on('readProperty', (data) => {
     }
 });
 
+// Event listener for 'writeProperty' requests
 client.on('writeProperty', (data) => {
+    console.log('Received writeProperty request:', data);
+
     const object = dataStore[data.request.objectId.type + ':' + data.request.objectId.instance];
     if (!object) return client.errorResponse(data.address, data.service, data.invokeId, Bacnet.enum.ErrorClasses.ERROR_CLASS_OBJECT, Bacnet.enum.ErrorCodes.ERROR_CODE_UNKNOWN_OBJECT);
     const property = object[data.request.property.id];
@@ -64,8 +72,10 @@ client.on('writeProperty', (data) => {
     }
 });
 
+// Event listener for 'whoHas' requests
 client.on('whoHas', (data) => {
-    console.log('whoHas', data);
+    console.log('Received whoHas request:', data);
+
     if (data.lowLimit && data.lowLimit > settings.deviceId) return;
     if (data.highLimit && data.highLimit < settings.deviceId) return;
     if (data.objId) {
@@ -80,15 +90,18 @@ client.on('whoHas', (data) => {
 });
 
 client.on('timeSync', (data) => {
+    console.log('Received timeSync request:', data);
     // TODO: Implement
 });
 
 client.on('timeSyncUTC', (data) => {
+    console.log('Received timeSyncUTC request:', data);
     // TODO: Implement
 });
 
 client.on('readPropertyMultiple', (data) => {
-    console.log('readPropertyMultiple', data.request.properties);
+    console.log('Received readPropertyMultiple request:', data);
+
     const responseList = [];
     const properties = data.request.properties;
     properties.forEach((property) => {
@@ -123,6 +136,7 @@ client.on('readPropertyMultiple', (data) => {
 });
 
 client.on('writePropertyMultiple', (data) => {
+    console.log('Received writePropertyMultiple request:', data);
     // TODO: Implement
     // TODO: valuesRefs
     //if () client.simpleAckResponse(data.address, data.service, data.invokeId);
@@ -130,35 +144,49 @@ client.on('writePropertyMultiple', (data) => {
 });
 
 client.on('atomicWriteFile', (data) => {
+    console.log('Received atomicWriteFile request:', data);
 });
 
 client.on('atomicReadFile', (data) => {
+    console.log('Received atomicReadFile request:', data);
 });
 
 client.on('subscribeCOV', (data) => {
+    console.log('Received subscribeCOV request:', data);
 });
 
 client.on('subscribeProperty', (data) => {
+    console.log('Received subscribeProperty request:', data);
 });
 
 client.on('deviceCommunicationControl', (data) => {
+    console.log('Received deviceCommunicationControl request:', data);
 });
 
 client.on('reinitializeDevice', (data) => {
+    console.log('Received reinitializeDevice request:', data);
 });
 
 client.on('readRange', (data) => {
+    console.log('Received readRange request:', data);
 });
 
 client.on('createObject', (data) => {
+    console.log('Received createObject request:', data);
 });
 
 client.on('deleteObject', (data) => {
+    console.log('Received deleteObject request:', data);
 });
 
-console.log('Node BACstack Device started');
-
+// Send a 'Who-Is' request every 2000 milliseconds (2 seconds)
 setInterval(() => {
     console.log('Sending Who-Is request');
-    client.whoIs();
+    client.whoIs({
+        lowLimit: 1,
+        highLimit: 1000
+    });
 }, 2000);
+
+// Log a message indicating the Node BACstack Device has started
+console.log('Node BACstack Device started');
